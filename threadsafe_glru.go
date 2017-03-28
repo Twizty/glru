6ca05d8a2bd903472d75709406254d941a9eeae2
Maxim Davydov <maximdavydoff@yandex.ru>
2017-03-28 17:23:06 +0300

    Added threadsafe implementation

---
+++ threadsafe_glru.go
@@ -0,0 +1,79 @@
+package glru
+
+import (
+  "time"
+)
+
+const (
+  CALCULATE_WITH_CATCH_ACTION = 1
+  CALCULATE_WITHOUT_CATCH_ACTION = 2
+  GET_COUNT = 3
+  EXIT = 999
+)
+
+type ThreadsafeLRUCache struct {
+  c chan *request
+}
+
+type request struct {
+  action       int
+  responsePipe chan<- interface{}
+  body         interface{}
+}
+
+func handleRequests(cache *LRUCache, c <-chan *request) {
+  for request := range c {
+    switch request.action {
+      case CALCULATE_WITH_CATCH_ACTION:
+        request.responsePipe <- cache.CalculateWithCache(request.body)
+      case CALCULATE_WITHOUT_CATCH_ACTION:
+        request.responsePipe <- cache.CalculateWithoutCache(request.body)
+      case GET_COUNT:
+        request.responsePipe <- cache.Count()
+      case EXIT:
+        return
+    }
+  }
+}
+
+func NewThreadsafeLRUCache(size int, chanSize int, calcFunc CalcFunc) *ThreadsafeLRUCache {
+  c := make(chan *request, chanSize)
+
+  go func() {
+    cache := NewLRUCache(size, calcFunc)
+
+    handleRequests(cache, c)
+  }()
+
+  return &ThreadsafeLRUCache{c}
+}
+
+func NewThreadsafeLRUCacheWithTimeout(size int, chanSize int, timeout time.Duration, calcFunc CalcFunc) *ThreadsafeLRUCache {
+  c := make(chan *request, chanSize)
+
+  go func() {
+    cache := NewLRUCacheWithTimeout(size, timeout, calcFunc)
+
+    handleRequests(cache, c)
+  }()
+
+  return &ThreadsafeLRUCache{c}
+}
+
+func (self *ThreadsafeLRUCache) CalculateWithCache(key interface{}) interface{} {
+  resultPipe := make(chan interface{})
+  self.c <- &request{CALCULATE_WITH_CATCH_ACTION, resultPipe, key}
+  return <-resultPipe
+}
+
+func (self *ThreadsafeLRUCache) CalculateWithoutCache(key interface{}) interface{} {
+  resultPipe := make(chan interface{})
+  self.c <- &request{CALCULATE_WITHOUT_CATCH_ACTION, resultPipe, key}
+  return <-resultPipe
+}
+
+func (self *ThreadsafeLRUCache) Count() int {
+  resultPipe := make(chan interface{})
+  self.c <- &request{GET_COUNT, resultPipe, nil}
+  return (<-resultPipe).(int)
+}
---
+++ threadsafe_glru_test.go
@@ -0,0 +1,36 @@
+package glru
+
+import (
+  "testing"
+  "time"
+)
+
+func TestThreadsafeCalculationWithCache(t *testing.T) {
+  c := NewThreadsafeLRUCache(10, 2, func (arg interface{}) interface{} {
+    return arg
+  })
+
+  go c.CalculateWithCache(5)
+  go c.CalculateWithCache(10)
+
+  time.Sleep(10 * time.Millisecond)
+  if c.Count() != 2 {
+    t.Error("Expected action to populate the queue twice, but got", c.Count())
+  }
+}
+
+func TestThreadsafeCalculationWithCacheOverflow(t *testing.T) {
+  c := NewThreadsafeLRUCache(3, 2, func (arg interface{}) interface{} {
+    return arg
+  })
+
+  go c.CalculateWithCache(5)
+  go c.CalculateWithCache(10)
+  go c.CalculateWithCache(15)
+  go c.CalculateWithCache(20)
+
+  time.Sleep(10 * time.Millisecond)
+  if c.Count() != 3 {
+    t.Error("Expected action to populate the queue twice, but got", c.Count())
+  }
+}
